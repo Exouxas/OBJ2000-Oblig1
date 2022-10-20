3be9fe1e44344c59acd302d33b8e2a2d7326b2e0
Exouxas <exouxas@gmail.com>
2022-10-20 12:30:39 +0200

    Create dummy tools

---
+++ src/main/java/com/example/oblig1/tools/DrawingTool.java
@@ -0,0 +1,22 @@
+package com.example.oblig1.tools;
+
+public class DrawingTool extends Tool{
+    public DrawingTool(){
+        super();
+    }
+
+    @Override
+    public void pressed(double x, double y) {
+
+    }
+
+    @Override
+    public void moved(double x, double y) {
+
+    }
+
+    @Override
+    public void released() {
+
+    }
+}
---
+++ src/main/java/com/example/oblig1/tools/SelectionTool.java
@@ -0,0 +1,22 @@
+package com.example.oblig1.tools;
+
+public class SelectionTool extends Tool{
+    public SelectionTool(){
+        super();
+    }
+
+    @Override
+    public void pressed(double x, double y) {
+
+    }
+
+    @Override
+    public void moved(double x, double y) {
+
+    }
+
+    @Override
+    public void released() {
+
+    }
+}
