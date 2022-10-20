17a8480938dc3e5659927df2544166dd76c331c9
Exouxas <exouxas@gmail.com>
2022-10-20 12:29:46 +0200

    Create basic abstract tool class

---
+++ src/main/java/com/example/oblig1/tools/Tool.java
@@ -0,0 +1,15 @@
+package com.example.oblig1.tools;
+
+import javafx.scene.shape.Shape;
+
+public abstract class Tool {
+    public Shape selected;
+
+    public Tool(){
+
+    }
+
+    public abstract void pressed(double x, double y);
+    public abstract void moved(double x, double y);
+    public abstract void released();
+}
