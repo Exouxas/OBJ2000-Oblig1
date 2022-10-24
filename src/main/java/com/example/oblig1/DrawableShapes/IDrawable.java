18af8289512cc0c7db28f6358bfd3510cfe52f29
Exouxas <exouxas@gmail.com>
2022-10-24 17:17:39 +0200

    Add methods to interface

--- src/main/java/com/example/oblig1/DrawableShapes/IDrawable.java
+++ src/main/java/com/example/oblig1/DrawableShapes/IDrawable.java
@@ -1,5 +1,30 @@
 package com.example.oblig1.DrawableShapes;
 
-public interface IDrawable {
+import javafx.geometry.Point2D;
 
+public interface IDrawable {
+    /**
+     * Gets the starting position of a drawn shape.
+     * @return
+     */
+    public Point2D getStart();
+
+    /**
+     * Sets the starting position of a drawn shape.
+     * @param point
+     */
+    public void setStart(Point2D point);
+
+
+    /**
+     * Gets the ending position of a drawn shape.
+     * @return
+     */
+    public Point2D getEnd();
+
+    /**
+     * Sets the ending position of a drawn shape.
+     * @param point
+     */
+    public void setEnd(Point2D point);
 }
