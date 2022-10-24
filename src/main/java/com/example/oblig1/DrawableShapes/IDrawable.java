421fd31abf299db17ff10ac4e5a43523bff8e3ef
Exouxas <exouxas@gmail.com>
2022-10-24 18:07:57 +0200

    Remove redundant access modifiers

--- src/main/java/com/example/oblig1/DrawableShapes/IDrawable.java
+++ src/main/java/com/example/oblig1/DrawableShapes/IDrawable.java
@@ -7,24 +7,24 @@ public interface IDrawable {
      * Gets the starting position of a drawn shape.
      * @return Starting position
      */
-    public Point2D getStart();
+    Point2D getStart();
 
     /**
      * Sets the starting position of a drawn shape.
      * @param point Starting position
      */
-    public void setStart(Point2D point);
+    void setStart(Point2D point);
 
 
     /**
      * Gets the ending position of a drawn shape.
      * @return Ending position
      */
-    public Point2D getEnd();
+    Point2D getEnd();
 
     /**
      * Sets the ending position of a drawn shape.
      * @param point Ending position
      */
-    public void setEnd(Point2D point);
+    void setEnd(Point2D point);
 }
