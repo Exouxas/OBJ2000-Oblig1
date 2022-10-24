a9c325e91e8070b59689cbbbb973365e76f65991
Exouxas <exouxas@gmail.com>
2022-10-24 10:51:19 +0200

    Update and implement IMoveable interface

--- src/main/java/com/example/oblig1/DrawableShapes/Drawable.java
+++ src/main/java/com/example/oblig1/DrawableShapes/Drawable.java
@@ -1,5 +1,12 @@
 package com.example.oblig1.DrawableShapes;
 
-public abstract class Drawable implements IMoveable, ISelectable {
+import javafx.geometry.Point2D;
 
+public abstract class Drawable implements IMoveable, ISelectable {
+    // IMoveable methods
+    public abstract Point2D getPosition();
+    public abstract void setPosition(Point2D position);
+    public void translate(Point2D magnitude){
+        setPosition(getPosition().add(magnitude));
+    }
 }
--- src/main/java/com/example/oblig1/DrawableShapes/IMoveable.java
+++ src/main/java/com/example/oblig1/DrawableShapes/IMoveable.java
@@ -1,4 +1,9 @@
 package com.example.oblig1.DrawableShapes;
 
+import javafx.geometry.Point2D;
+
 public interface IMoveable {
+    public void translate(Point2D magnitude);
+    public Point2D getPosition();
+    public void setPosition(Point2D position);
 }
