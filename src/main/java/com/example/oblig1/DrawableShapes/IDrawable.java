f07401e7a11064162e7555dd6e23e2995e339e5b
Exouxas <exouxas@gmail.com>
2022-10-24 17:18:54 +0200

    Add more text to javadoc

--- src/main/java/com/example/oblig1/DrawableShapes/IDrawable.java
+++ src/main/java/com/example/oblig1/DrawableShapes/IDrawable.java
@@ -5,26 +5,26 @@ import javafx.geometry.Point2D;
 public interface IDrawable {
     /**
      * Gets the starting position of a drawn shape.
-     * @return
+     * @return Starting position
      */
     public Point2D getStart();
 
     /**
      * Sets the starting position of a drawn shape.
-     * @param point
+     * @param point Starting position
      */
     public void setStart(Point2D point);
 
 
     /**
      * Gets the ending position of a drawn shape.
-     * @return
+     * @return Ending position
      */
     public Point2D getEnd();
 
     /**
      * Sets the ending position of a drawn shape.
-     * @param point
+     * @param point Ending position
      */
     public void setEnd(Point2D point);
 }
