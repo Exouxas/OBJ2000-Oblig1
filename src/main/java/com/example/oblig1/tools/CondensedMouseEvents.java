c5e4f342656c1ddc4eba86d7179d34f8d4a0140f
Exouxas <exouxas@gmail.com>
2022-10-25 19:32:44 +0200

    Create class to keep useful lambdas for mouse events

---
+++ src/main/java/com/example/oblig1/tools/CondensedMouseEvents.java
@@ -0,0 +1,12 @@
+package com.example.oblig1.tools;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
+public class CondensedMouseEvents {
+    public EventHandler<MouseEvent> pressed;
+    public EventHandler<MouseEvent> dragged;
+    public EventHandler<MouseEvent> released;
+
+    public CondensedMouseEvents(){}
+}
